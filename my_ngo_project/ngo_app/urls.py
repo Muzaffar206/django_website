dc34d5a085e8a800263e416491105d144c03c200
Muzaffar206 <shaikhmuzaffar206@gmail.com>
2024-09-09 00:08:04 +0530

    added the HTML template

--- my_ngo_project/ngo_app/urls.py
+++ my_ngo_project/ngo_app/urls.py
@@ -3,4 +3,39 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('about/', views.about, name='about'),
+    path('about/mesco-at-glance/', views.about_glance, name='about_glance'),
+    path('about/managing-committee/', views.managing_committee, name='managing_committee'),
+    path('about/good-governance/', views.good_governance, name='good_governance'),
+    path('about/mesco-education-society/', views.mesco_education_society, name='mesco_education_society'),
+    path('about/angels-paradise-nursery/', views.angels_paradise_nursery, name='angels_paradise_nursery'),
+    path('about/vocational-training/', views.vocational_training, name='vocational_training'),
+    path('about/ecce/', views.ecce, name='ecce'),
+
+    path('our-work/', views.our_work, name='our_work'),
+    path('our-work/educational-support/', views.educational_support, name='educational_support'),
+    path('our-work/medical-aid/', views.medical_aid, name='medical_aid'),
+    path('our-work/clinics-dispensaries/', views.clinics_dispensaries, name='clinics_dispensaries'),
+    path('our-work/school-adoption/', views.school_adoption, name='school_adoption'),
+    path('our-work/newspaper-units/', views.newspaper_units, name='newspaper_units'),
+
+    path('media/', views.media, name='media'),
+    path('media/in-media/', views.in_media, name='in_media'),
+    path('media/image-gallery/', views.image_gallery, name='image_gallery'),
+    path('media/video-gallery/', views.video_gallery, name='video_gallery'),
+    path('media/newsletters/', views.newsletters, name='newsletters'),
+    path('media/annual-reports/', views.annual_reports, name='annual_reports'),
+    path('media/success-stories/', views.success_stories, name='success_stories'),
+    path('media/donor-testimonials/', views.donor_testimonials, name='donor_testimonials'),
+    path('blog/', views.blog, name='blog'),
+
+    path('contact/', views.contact, name='contact'),
+    path('login/', views.login, name='login'),
+    path('register/', views.register, name='register'),
+    path('faq/', views.faq, name='faq'),
+    path('terms-conditions/', views.terms_conditions, name='terms_conditions'),
+    path('privacy-policy/', views.privacy_policy, name='privacy_policy'),
+    path('volunteer/', views.volunteer, name='volunteer'),
+    path('donate/', views.donate, name='donate'),
+
 ]
--- my_ngo_project/ngo_app/views.py
+++ my_ngo_project/ngo_app/views.py
@@ -2,3 +2,100 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'home.html')
+# About Pages
+def about(request):
+    return render(request, 'about/about.html')
+
+def about_glance(request):
+    return render(request, 'about/mesco_at_glance.html')
+
+def managing_committee(request):
+    return render(request, 'about/managing_committee.html')
+
+def good_governance(request):
+    return render(request, 'about/good_governance.html')
+
+# Add similar views for the rest of the about, our work, and media pages
+def mesco_education_society(request):
+    return render(request, 'about/mesco_education_society.html')
+
+def angels_paradise_nursery(request):
+    return render(request, 'about/angels_paradise_nursery.html')
+
+def vocational_training(request):
+    return render(request, 'about/vocational_training.html')
+
+def ecce(request):
+    return render(request, 'about/ecce.html')
+
+# Our Work Pages
+def our_work(request):
+    return render(request, 'our_work/our_work.html')
+
+def educational_support(request):
+    return render(request, 'our_work/educational_support.html')
+
+def medical_aid(request):
+    return render(request, 'our_work/medical_aid.html')
+
+def clinics_dispensaries(request):
+    return render(request, 'our_work/clinics_dispensaries.html')
+
+def school_adoption(request):
+    return render(request, 'our_work/school_adoption.html')
+
+def newspaper_units(request):
+    return render(request, 'our_work/newspaper_units.html')
+
+# Media Pages
+def media(request):
+    return render(request, 'media/media.html')
+
+def in_media(request):
+    return render(request, 'media/in_media.html')
+
+def image_gallery(request):
+    return render(request, 'media/image_gallery.html')
+
+def video_gallery(request):
+    return render(request, 'media/video_gallery.html')
+
+def newsletters(request):
+    return render(request, 'newsletters.html')
+
+def annual_reports(request):
+    return render(request, 'annual_reports.html')
+
+def success_stories(request):
+    return render(request, 'success_stories.html')
+
+def donor_testimonials(request):
+    return render(request, 'donor_testimonials.html')
+
+# Contact, Login, Register, FAQ, Terms, etc.
+def contact(request):
+    return render(request, 'contact.html')
+
+def login(request):
+    return render(request, 'login.html')
+
+def register(request):
+    return render(request, 'register.html')
+
+def faq(request):
+    return render(request, 'faq.html')
+
+def terms_conditions(request):
+    return render(request, 'terms_conditions.html')
+
+def privacy_policy(request):
+    return render(request, 'privacy_policy.html')
+
+def volunteer(request):
+    return render(request, 'volunteer.html')
+
+def donate(request):
+    return render(request, 'donate.html')
+
+def blog(request):
+    return render(request, 'blog.html')
