bf5002ce603d7b277e2dd608bed81d1cc31f7f66
Muzaffar206 <shaikhmuzaffar206@gmail.com>
2024-09-15 18:03:13 +0530

    dataabase change from sqlite3 to MYSQL database

--- my_ngo_project/my_ngo_project/settings.py
+++ my_ngo_project/my_ngo_project/settings.py
@@ -83,10 +83,15 @@ WSGI_APPLICATION = 'my_ngo_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'ngo_website',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': 'localhost',  
+        'PORT': '3306',      
     }
 }
+
 
 
 # Password validation
